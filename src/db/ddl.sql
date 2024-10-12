7e62435243e958c767ca7c3c630b09f3904c9be9
sui <1202ayasnppk@gmail.com>
2024-10-12 17:08:58 +0900

    update: テーブル定義最新化

--- src/db/ddl.sql
+++ src/db/ddl.sql
@@ -1,53 +1,44 @@
--- 都道府県テーブルの削除
-DROP TABLE IF EXISTS population_data CASCADE; 
+DROP TABLE IF EXISTS m_prefecture CASCADE; 
 
-DROP TABLE IF EXISTS city CASCADE; 
-
-DROP TABLE IF EXISTS prefecture CASCADE; 
-
--- 都道府県テーブルの作成
-CREATE TABLE prefecture( 
+CREATE TABLE m_prefecture( 
     pref_code VARCHAR (5) PRIMARY KEY
     , pref_name VARCHAR (50) NOT NULL
 ); 
 
--- 都道府県テーブルへのコメント追加
 COMMENT 
-    ON TABLE prefecture IS '都道府県の基本情報を保持するテーブル'; 
-
--- 都道府県テーブルのカラムにコメント追加
-COMMENT 
-    ON COLUMN prefecture.pref_code IS '都道府県コード'; 
+    ON TABLE m_prefecture IS '都道府県マスタ'; 
 
 COMMENT 
-    ON COLUMN prefecture.pref_name IS '都道府県名'; 
+    ON COLUMN m_prefecture.pref_code IS '都道府県コード'; 
 
--- 市区町村テーブルの作成
-CREATE TABLE city( 
+COMMENT 
+    ON COLUMN m_prefecture.pref_name IS '都道府県名'; 
+
+DROP TABLE IF EXISTS m_city CASCADE; 
+
+CREATE TABLE m_city( 
     city_code VARCHAR (10) PRIMARY KEY
     , city_name VARCHAR (100) NOT NULL
     , pref_code VARCHAR (5) NOT NULL
-    , FOREIGN KEY (pref_code) REFERENCES prefecture(pref_code)
+    , FOREIGN KEY (pref_code) REFERENCES m_prefecture(pref_code)
 ); 
 
--- 市区町村テーブルへのコメント追加
 COMMENT 
-    ON TABLE city IS '市区町村の基本情報を保持するテーブル'; 
-
--- 市区町村テーブルのカラムにコメント追加
-COMMENT 
-    ON COLUMN city.city_code IS '市区町村コード'; 
+    ON TABLE m_city IS '市区町村マスタ'; 
 
 COMMENT 
-    ON COLUMN city.city_name IS '市区町村名'; 
+    ON COLUMN m_city.city_code IS '市区町村コード'; 
 
 COMMENT 
-    ON COLUMN city.pref_code IS '都道府県コード'; 
+    ON COLUMN m_city.city_name IS '市区町村名'; 
 
--- 人口データテーブルの作成
-CREATE TABLE population_data( 
+COMMENT 
+    ON COLUMN m_city.pref_code IS '都道府県コード'; 
+
+DROP TABLE IF EXISTS m_population_data CASCADE; 
+
+CREATE TABLE m_population_data( 
     city_code VARCHAR (10) NOT NULL
-    , pref_code VARCHAR (5) NOT NULL
     , census_year INT NOT NULL
     , total_population INT NOT NULL
     , male_total_population INT NOT NULL
@@ -62,56 +53,50 @@ CREATE TABLE population_data(
     , female_population_15_to_64 INT NOT NULL
     , female_population_over_65 INT NOT NULL
     , PRIMARY KEY (city_code, census_year)
-    , FOREIGN KEY (city_code) REFERENCES city(city_code)
-    , FOREIGN KEY (pref_code) REFERENCES prefecture(pref_code)
+    , FOREIGN KEY (city_code) REFERENCES m_city(city_code)
 ); 
 
--- 人口データテーブルへのコメント追加
 COMMENT 
-    ON TABLE population_data IS '市区町村と調査年ごとの詳細な人口データを保持するテーブル'; 
-
--- 人口データテーブルのカラムにコメント追加
-COMMENT 
-    ON COLUMN population_data.city_code IS '市区町村コード'; 
+    ON TABLE m_population_data IS '人口動態マスタ'; 
 
 COMMENT 
-    ON COLUMN population_data.pref_code IS '都道府県コード'; 
+    ON COLUMN m_population_data.city_code IS '市区町村コード'; 
 
 COMMENT 
-    ON COLUMN population_data.census_year IS '調査年'; 
+    ON COLUMN m_population_data.census_year IS '調査年'; 
 
 COMMENT 
-    ON COLUMN population_data.total_population IS '総人口'; 
+    ON COLUMN m_population_data.total_population IS '総人口'; 
 
 COMMENT 
-    ON COLUMN population_data.male_total_population IS '男性総人口'; 
+    ON COLUMN m_population_data.male_total_population IS '男性総人口'; 
 
 COMMENT 
-    ON COLUMN population_data.female_total_population IS '女性総人口'; 
+    ON COLUMN m_population_data.female_total_population IS '女性総人口'; 
 
 COMMENT 
-    ON COLUMN population_data.population_under_15 IS '15歳未満人口'; 
+    ON COLUMN m_population_data.population_under_15 IS '15歳未満人口'; 
 
 COMMENT 
-    ON COLUMN population_data.population_15_to_64 IS '15～64歳人口'; 
+    ON COLUMN m_population_data.population_15_to_64 IS '15～64歳人口'; 
 
 COMMENT 
-    ON COLUMN population_data.population_over_65 IS '65歳以上人口'; 
+    ON COLUMN m_population_data.population_over_65 IS '65歳以上人口'; 
 
 COMMENT 
-    ON COLUMN population_data.male_population_under_15 IS '男性15歳未満人口'; 
+    ON COLUMN m_population_data.male_population_under_15 IS '男性15歳未満人口'; 
 
 COMMENT 
-    ON COLUMN population_data.male_population_15_to_64 IS '男性15～64歳人口'; 
+    ON COLUMN m_population_data.male_population_15_to_64 IS '男性15～64歳人口'; 
 
 COMMENT 
-    ON COLUMN population_data.male_population_over_65 IS '男性65歳以上人口'; 
+    ON COLUMN m_population_data.male_population_over_65 IS '男性65歳以上人口'; 
 
 COMMENT 
-    ON COLUMN population_data.female_population_under_15 IS '女性15歳未満人口'; 
+    ON COLUMN m_population_data.female_population_under_15 IS '女性15歳未満人口'; 
 
 COMMENT 
-    ON COLUMN population_data.female_population_15_to_64 IS '女性15～64歳人口'; 
+    ON COLUMN m_population_data.female_population_15_to_64 IS '女性15～64歳人口'; 
 
 COMMENT 
-    ON COLUMN population_data.female_population_over_65 IS '女性65歳以上人口';
+    ON COLUMN m_population_data.female_population_over_65 IS '女性65歳以上人口';
