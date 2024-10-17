ac432c94efc48cd9130cd436b573ba0f4b128747
sui <1202ayasnppk@gmail.com>
2024-10-18 07:58:31 +0900

    add: 市区町村の検索バーを追加

--- src/components/chart-data.tsx
+++ src/components/chart-data.tsx
@@ -1,8 +1,10 @@
 'use client';
+import { Input } from '@/components/ui/input';
 import type { Municipality } from '@/types';
 import { useSearchParams } from 'next/navigation';
-import { useEffect, useState } from 'react';
+import { useEffect, useMemo, useState } from 'react';
 import { ChartComponents } from './line-chart';
+import { Button } from './ui/button';
 import { Checkbox } from './ui/checkbox';
 import { Label } from './ui/label';
 
@@ -11,10 +13,40 @@ type Props = {
 };
 
 export default function ChartData({ params }: Props) {
-  // 選択された市区町村を管理するステート
   const [selectedMunicipalities, setSelectedMunicipalities] = useState<Municipality[]>([]);
+  const [searchMunicipalities, setSearchMunicipalities] = useState<string>('');
+  const [currentIndex, setCurrentIndex] = useState<number>(20);
 
   const searchParams = useSearchParams();
+
+  const filteredMunicipalities = useMemo(
+    () =>
+      params.filter((param) =>
+        param.municipalityName.toLowerCase().includes(searchMunicipalities.toLowerCase()),
+      ),
+    [searchMunicipalities, params],
+  );
+
+  const displayedMunicipalities = useMemo(
+    () => filteredMunicipalities.slice(0, currentIndex),
+    [filteredMunicipalities, currentIndex],
+  );
+
+  const showMore = useMemo(
+    () =>
+      (searchMunicipalities === '' ? params.length : filteredMunicipalities.length) > currentIndex,
+    [searchMunicipalities, params.length, filteredMunicipalities.length, currentIndex],
+  );
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const value = event.target.value;
+    setSearchMunicipalities(value);
+    setCurrentIndex(10);
+  }
+
+  function handleShowMore() {
+    setCurrentIndex(currentIndex + 10);
+  }
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: <explanation>
   useEffect(() => {
@@ -34,8 +66,19 @@ export default function ChartData({ params }: Props) {
 
   return (
     <div>
+      <div className='mb-4'>
+        <Input
+          type='text'
+          name='municipality'
+          value={searchMunicipalities}
+          onChange={handleInputChange}
+          placeholder='市区町村を検索...'
+          autoComplete='address-level2'
+          className='w-full'
+        />
+      </div>
       <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-4'>
-        {params.map((municipality) => {
+        {displayedMunicipalities.map((municipality) => {
           const isChecked = selectedMunicipalities.some(
             (m) => m.municipalityCode === municipality.municipalityCode,
           );
@@ -58,6 +101,11 @@ export default function ChartData({ params }: Props) {
           );
         })}
       </div>
+      {showMore && (
+        <Button variant='default' className='mt-4 w-full' onClick={handleShowMore}>
+          市区町村を読み込む...
+        </Button>
+      )}
       {selectedMunicipalities.length > 0 && <ChartComponents params={selectedMunicipalities} />}
     </div>
   );
